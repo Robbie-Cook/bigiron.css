dfa0b5921a37daa96693af344c9ad8164b510cf6
Robbie Cook <robbieck7@gmail.com>
2020-12-09 20:43:52 +1300

    feat: adding javascript method for adjusting theme

---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+export declare const lightProperties: Record<string, any>;
+export declare const darkProperties: Record<string, any>;
+/**
+ * Set a theme in CSS (light or dark)
+ */
+export declare function setTheme(mode: "light" | "dark"): void;
---
+++ src/index.ts
@@ -0,0 +1,37 @@
+// @ts-ignore
+import lightVariables from './variables-light.css'
+// @ts-ignore
+import darkVariables from './variables-light.css'
+import css from "css";
+
+
+/**
+ * Get the CSS variables from a theme
+ */
+function getCssVariablesObject(str: string): Record<string, any> {
+	// @ts-ignore
+	const decs = css.parse(str)?.stylesheet?.rules[0]?.declarations;
+	let obj: {[T: string]: any} = {};
+	for (const dec of decs) {
+		obj[dec.property] = dec.value;
+	}
+	return obj;
+}
+
+export const lightProperties = getCssVariablesObject(lightVariables);
+export const darkProperties = getCssVariablesObject(darkVariables);
+
+/**
+ * Set a theme in CSS (light or dark)
+ */
+export function setTheme(mode: "light" | "dark") {
+	let data = lightProperties;
+  if (mode === "dark") {
+    data = darkProperties;
+  }
+
+  const dataEntries = Object.entries(data);
+  for (const [key, value] of dataEntries) {
+    document.body.style.setProperty(key, value);
+  }
+}
