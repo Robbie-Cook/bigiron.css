24a2f5284f5177204d3bdc5e06076a4826493634
Robbie Cook <robbie.cook@sendpayments.com>
2021-07-04 00:32:27 +1200

    feat: adding dom node to setTheme

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,20 @@
 // @ts-ignore
-import lightVariables from './variables-light.scss'
+import lightVariables from './variables-light.scss';
 // @ts-ignore
-import darkVariables from './variables-dark.scss'
-import css from "css";
-
+import darkVariables from './variables-dark.scss';
+import css from 'css';
 
 /**
  * Get the CSS variables from a theme
  */
 function getCssVariablesObject(str: string): Record<string, any> {
-	// @ts-ignore
-	const decs = css.parse(str)?.stylesheet?.rules[0]?.declarations;
-	let obj: {[T: string]: any} = {};
-	for (const dec of decs) {
-		obj[dec.property] = dec.value;
-	}
-	return obj;
+  // @ts-ignore
+  const decs = css.parse(str)?.stylesheet?.rules[0]?.declarations;
+  let obj: { [T: string]: any } = {};
+  for (const dec of decs) {
+    obj[dec.property] = dec.value;
+  }
+  return obj;
 }
 
 export const lightProperties = getCssVariablesObject(lightVariables);
@@ -24,16 +23,17 @@ export const darkProperties = getCssVariablesObject(darkVariables);
 /**
  * Set a theme in CSS (light or dark)
  */
-export function setTheme(mode: "light" | "dark") {
-	let data = lightProperties;
-  if (mode === "dark") {
-		data = darkProperties;
-	}
-	
-	console.log(darkProperties);
-	
+export function setTheme(
+  mode: 'light' | 'dark',
+  domElement: HTMLElement = document.body
+) {
+  let data = lightProperties;
+  if (mode === 'dark') {
+    data = darkProperties;
+  }
+
   const dataEntries = Object.entries(data);
   for (const [key, value] of dataEntries) {
-		document.body.style.setProperty(key, value);
+    domElement.style.setProperty(key, value);
   }
 }
